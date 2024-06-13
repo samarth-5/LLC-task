65ae41a535245e4fbb783414c5ad93a7e587476e
Samarth Narayan <kayaniv321@gmail.com>
2024-06-13 23:28:56 +0530

    final changes

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -30,14 +30,15 @@
 
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import User from './pages/User.jsx';
 import Dashboard from './pages/Dashboard.jsx';
+import User from './pages/User.jsx';
 
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path='/' element={<User />} />
       <Route path='/user' element={<User />} />
       <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
